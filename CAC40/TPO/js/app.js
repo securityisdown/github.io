28ab334aa1d4127f421cfdd6291125ad316110f6
securityisdown <85262806+securityisdown@users.noreply.github.com>
2022-10-07 19:50:50 -0300

    Add files via upload

---
+++ CAC40/TPO/js/app.js
@@ -0,0 +1,5 @@
+function onLoad() {
+    document.querySelector("header").innerHTML=header
+    document.querySelector("footer").innerHTML=footer
+    document.querySelector("nav").innerHTML=nav
+}
---
+++ CAC40/TPO/js/templates.js
@@ -0,0 +1,34 @@
+let header=`
+<div class="cabecera">
+<div class="hijo">
+    <img class="logo" src="https://barilocheturismo.gob.ar/images/banco_logos/32_screen-shot-2018-06-07-at-083446.png?1663022437" alt="Logo">
+</div>
+<div class="hijo">
+    <span class="titulo">Aventuras en Bariloche</span>
+</div>
+<div class="hijo">
+    <div id="cont_b686a253e101e707257b83d6110dc66e"></div>
+</div>
+</div>
+`
+let footer=`
+<div class="contenedor chiqui">
+<div class="hijo">Copyright 2022</div>
+<div class="hijo centro">Powered by: CAC 4.0</div>
+<div class="hijo der">Version: 1.0</div>
+</div>
+`
+let nav=`
+<div class="menu">
+<div class="hijo">
+    <ul>
+        <li><a href="index.html">Inicio</a></li>
+        <li><a href="#">Historia</a></li>
+        <li><a href="#">Como llegar?</a></li>
+        <li><a href="#">Qué visitar?</a></li>
+        <li><a href="#">Circuitos</a></li>
+        <li><a href="contacto.html">Contáctenos</a></li>
+    </ul>
+</div>
+</div>
+`
