37a783b4cb3689614a847dcde7b782036da82ee0
securityisdown <85262806+securityisdown@users.noreply.github.com>
2022-10-24 14:04:26 -0300

    Update templates.js

--- CAC40/TPO/js/templates.js
+++ CAC40/TPO/js/templates.js
@@ -3,8 +3,8 @@ let header=`
 <div class="hijo">
     <img class="logo" src="https://barilocheturismo.gob.ar/images/banco_logos/32_screen-shot-2018-06-07-at-083446.png?1663022437" alt="Logo">
 </div>
-<div class="hijo">
-    <span class="titulo centrar">Aventuras en Bariloche</span>
+<div class="hijo centrado">
+    <span class="titulo">Aventuras en Bariloche</span>
 </div>
 <div class="hijo">
     <div id="cont_b686a253e101e707257b83d6110dc66e"></div>
